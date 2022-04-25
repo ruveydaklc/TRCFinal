0c0c685524733b94663f6edeac9b653634ba7b61
ruveydakilic <73518354+ruveydakilic@users.noreply.github.com>
2022-04-25 12:14:42 +0300

    commit

--- app/src/main/java/com/example/tripplanner/bll/TripPlannerLogic.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tripplanner.bll
-
-class TripPlannerLogic {
-}
--- app/src/main/java/com/example/tripplanner/view/fragments/YerEkleFragment.kt
+++ app/src/main/java/com/example/tripplanner/view/fragments/YerEkleFragment.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tripplanner.R
+import com.example.tripplanner.databinding.FragmentYerEkleBinding
 
 /** Gezilecek Yer Ekleme Fragment*/
 class YerEkleFragment : Fragment() {
 
+    lateinit var binding: FragmentYerEkleBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_yer_ekle, container, false)
+        binding= FragmentYerEkleBinding.inflate(inflater)
+
+
+
+
+
+        return binding.root
+
     }
 
 
